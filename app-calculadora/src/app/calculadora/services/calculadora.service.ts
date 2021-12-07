83cf42c51da8c819788eaa0889e5fa0c2e3c3af4
Jefferson Santos <jeff.debian2016@gmail.com>
2021-12-07 17:59:24 -0300

    Criando a camada de serviços.

--- app-calculadora/src/app/calculadora/services/calculadora.service.ts
+++ app-calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculadoraService {
+  constructor() {}
 
-constructor() { }
+  private readonly SOMA: string = '+';
+  private readonly SUBTRACAO: string = '-';
+  private readonly DIVISAO: string = '/';
+  private readonly MULTIPLICACAO: string = '*';
 
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number;
+
+    switch (operacao) {
+      case this.SOMA:
+        resultado = num1 + num2;
+        break;
+      case this.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case this.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case this.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+    return resultado;
+  }
 }
